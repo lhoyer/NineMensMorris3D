fb7a5f0c3ec1fb299ecdedf3a670c80516984191
Lukas Hoyer <lukasvii@live.de>
2014-10-05 17:27:52 +0200

    add 2 point crossover and new parent selection algorithm

--- Evolution/Evolution.js
+++ Evolution/Evolution.js
@@ -8,41 +8,86 @@ Evolution.prototype.newGeneration = function() {
 	var childrenNumber = [10,7,5,5,4,3,2,2,1,1];
 	var children = [];
 
-	for (var i = 0; i < 10; i++) {
-		parents[i] = this.estCSet[i];
+	for (var i = 0; i < childrenNumber.length; i++) {
+		parents.push({c:this.estCSet[i],n:childrenNumber[i]});
 		//use crossover to copy parent and give him the right generation id
-		children.push(this.crossover(parents[i],parents[i]));
-		children[children.length-1].parents = parents[i].id;
+		children.push(this.crossover2P(parents[i].c,parents[i].c)[0]);
+		children[children.length-1].parents = parents[i].c;
 	}
-
-	for (var i = 0; i < parents.length; i++) {
-		for (var j = 0; j < childrenNumber[i]; j++) {
-			var p1 = i;
-			do {
-				var p2 = Math.floor(Math.random() * parents.length)
-			} while (p1 === p2)
-			children.push(this.crossover(parents[p1],parents[p2]));
+	while(1) {
+		var randomSet = [];
+		for (var i = 0; i < parents.length; i++) {
+			if (parents[i].n > 0)
+				randomSet.push(parents[i]);
 		}
+		//if there is only one parent left, choose the other randomly
+		if (randomSet.length === 1) {
+			randomSet.push(parents[Math.floor(Math.random()*parents.length)]);
+		}
+		if (randomSet.length === 0)
+			break;
+
+		var p1 = 0;
+		var p2 = Math.floor((Math.random() * (randomSet.length-1))+1);
+
+		var tmpC = this.crossover2P(randomSet[p1].c,randomSet[p2].c);
+		children.push(tmpC[0]);
+		children.push(tmpC[1]);
+		randomSet[p1].n--;
+		randomSet[p2].n--;
 	}
 
 	return children;
 };
 
-Evolution.prototype.crossover = function(p1,p2) {
-	var c = new EstCoefficient();
+Evolution.prototype.crossover1P = function(p1,p2) {
+	var c1 = new EstCoefficient();
+	var c2 = new EstCoefficient();
 	//crossover point between 1 and 17 -> minimum one coefficient from one parent
 	var crossoverPoint = Math.floor(Math.random()*17)+1;
 
 	for (var i = 0; i < 18; i++) {
-		if (i < crossoverPoint)
-			this.copyCoefficient(p1,c,i);
-		else
-			this.copyCoefficient(p2,c,i);
+		if (i < crossoverPoint) {
+			this.copyCoefficient(p1,c1,i);
+			this.copyCoefficient(p2,c2,i);
+		}
+		else {
+			this.copyCoefficient(p2,c1,i);
+			this.copyCoefficient(p1,c2,i);
+		}
 	}
-	c.id = "G" + (Generations.lastGeneration+1);
-	c.parents = p1.id + p2.id;
+	c1.id = "G" + (Generations.lastGeneration+1);
+	c1.parents = p1.id + p2.id;
+	c2.id = "G" + (Generations.lastGeneration+1);
+	c2.parents = p2.id + p1.id;
 	
-	return c;
+	return [c1,c2];
+};
+
+Evolution.prototype.crossover2P = function(p1,p2) {
+	var c1 = new EstCoefficient();
+	var c2 = new EstCoefficient();
+	//crossover point between 1 and 17 -> minimum one coefficient from one parent
+	var crossoverPoint1 = Math.floor(Math.random()*17)+1;
+	var crossoverPoint2 = Math.floor(Math.random()*17)+1;
+
+	for (var i = 0; i < 18; i++) {
+		if (crossoverPoint1 < i && i < crossoverPoint2 ||
+			crossoverPoint2 < i && i < crossoverPoint1) {
+			this.copyCoefficient(p1,c1,i);
+			this.copyCoefficient(p2,c2,i);
+		}
+		else {
+			this.copyCoefficient(p2,c1,i);
+			this.copyCoefficient(p1,c2,i);
+		}
+	}
+	c1.id = "G" + (Generations.lastGeneration+1);
+	c1.parents = p1.id + p2.id;
+	c2.id = "G" + (Generations.lastGeneration+1);
+	c2.parents = p2.id + p1.id;
+	
+	return [c1,c2];
 };
 
 Evolution.prototype.copyCoefficient = function(src,dest,idx) {
